e5fb5e01905d9373536beb13b05a6afba9200ce4
neytoe <nanyankah@gmail.com>
2021-09-25 14:27:47 +0100

    Add dockerfile

--- PlatformService/Platformservice/obj/Debug/net5.0/Platformservice.AssemblyInfo.cs
+++ PlatformService/Platformservice/obj/Debug/net5.0/Platformservice.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a99e6a3f-b15c-4017-85ba-05cf72463e91")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Platformservice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
