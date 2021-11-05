e1ffe29e4fdceaae320410577af7bc3be956a58d
neytoe <nanyankah@gmail.com>
2021-11-05 10:49:37 +0100

    changes to PrepDb class

--- PlatformService/Platformservice/Data/prepDb.cs
+++ PlatformService/Platformservice/Data/prepDb.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Platformservice.Models;
 using System;
@@ -10,16 +11,28 @@ namespace Platformservice.Data
 {
     public static class prepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app , bool isProd)
         {
             using(var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
             }
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProd)
         {
+            if (!isProd) {
+                Console.WriteLine("---> Attempting to apply migrations....");
+                try
+                {
+
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Could not run migrations because {e.Message}");
+                }
+            }
             if (!context.Platforms.Any())
             {
                 context.Platforms.AddRange(
