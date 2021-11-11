9591bcf065b7843947da062172ba8f7018eacc73
neytoe <nanyankah@gmail.com>
2021-11-11 23:46:18 +0100

    add models for commandservice

--- CommandsService/Models/Command.cs
+++ CommandsService/Models/Command.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace CommandsService.Models
 {
-    public class Platform
-    {   
+    public class Command
+    {
         [Key]
         [Required]
         public int Id { get; set; }
         [Required]
-        public int ExternalId { get; set; }
+        public string HowTo { get; set; }
         [Required]
-        public string Name { get; set; }
-        public ICollection<Command> Commands { get; set; } = new List<Command>();
+        public string CommandLine  { get; set; }
+        [Required]
+        public int PlatformId { get; set; }
+        public Platform Platform { get; set; }
     }
 }
