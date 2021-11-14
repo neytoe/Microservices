1ee4c821a97a3d4f75eb1cd03014739ed0e86a84
neytoe <nanyankah@gmail.com>
2021-11-14 10:11:03 +0100

    add the commandRepo interface

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,23 @@
+﻿using CommandsService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        //Platforms
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlatformExists(int PlatformId);
+
+        //Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
