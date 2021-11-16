a8cdbd72f567a75a268d1212276e14ff1cbd86cf
neytoe <nanyankah@gmail.com>
2021-11-16 11:16:46 +0100

    complete commandController

--- CommandsService/Controllers/CommandsController.cs
+++ CommandsService/Controllers/CommandsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CommandsService.Data;
 using CommandsService.Dtos;
+using CommandsService.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -32,7 +33,38 @@ namespace CommandsService.Controllers
                 return NotFound();
 
             var commands = _repository.GetCommandsForPlatform(platformId);
-            var result = _mapper.Map()
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name ="GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatfromId(int platformId, int commandId)
+        {
+            Console.WriteLine($"-----> Hit GetCommandsForPlatform {platformId} and Command {commandId}");
+
+            if (!_repository.PlatformExists(platformId))
+                return NotFound();
+
+            var command = _repository.GetCommand(platformId, commandId);
+
+            if (command == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
+        }
+
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(int platformId, CommandCreateDto newCommand)
+        {
+            Console.WriteLine($"-----> Hit CreateCommand with PlatformID {platformId}");
+
+            if (!_repository.PlatformExists(platformId))
+                return NotFound();
+
+            var command = _mapper.Map<Command>(newCommand);
+            _repository.CreateCommand(platformId, command);
+            _repository.SaveChanges();
+
+            return Ok(_mapper.Map<CommandReadDto>(command));
         }
     }
 }
