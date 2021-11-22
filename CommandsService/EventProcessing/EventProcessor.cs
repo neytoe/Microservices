813ae62bd8618ddded7430b5666b2d8707863515
neytoe <nanyankah@gmail.com>
2021-11-22 23:49:13 +0100

    implement eventprocessor class

--- CommandsService/EventProcessing/EventProcessor.cs
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -1,19 +1,79 @@
-﻿using System;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CommandsService.EventProcessing
 {
     public class EventProcessor : IEventProcessor
     {
-        public EventProcessor()
-        {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
 
+        public EventProcessor(IServiceScopeFactory scopeFactory,
+            IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
         }
         public void ProcessEvent(string message)
         {
-            throw new NotImplementedException();
+            var eventType = DetermineEvent(message);
+            
+            switch (eventType)
+            {
+                case EventType.PlatformPublished:
+                    //TODO
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            Console.WriteLine($"----> Determining Event");
+                                                        //--source     --Target
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("----> could not determine EventType");
+                    return EventType.Undetermined;
+            }
+        }
+
+        private void addPlatform(string platformPublishedMesssage)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetRequiredService<ICommandRepo>();
+
+                var platformPublishedDto = JsonSerializer.Deserialize<PlatformPublishedDto>(platformPublishedMesssage);
+                try
+                {
+                    var plat = _mapper.Map<Platform>(platformPublishedDto);
+                    if (!repo.ExternalPlatformExists(plat.ExternalId))
+                        repo.CreatePlatform(plat);
+                    else
+                        Console.WriteLine($"Platform already exists!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"---> Could not add platform to DB {ex.Message}");
+                }
+                
+            }
         }
     }
 
